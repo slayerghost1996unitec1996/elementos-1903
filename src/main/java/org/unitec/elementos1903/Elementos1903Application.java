f378f4d7f26edd2a871e8dea32cc8f2e2e88a3c3
Servicios Especiales <Servicios Especiales@SE11>
2019-06-04 21:30:08 -0500

    clase 5

--- src/main/java/org/unitec/elementos1903/Elementos1903Application.java
+++ src/main/java/org/unitec/elementos1903/Elementos1903Application.java
@@ -1,13 +1,41 @@
 package org.unitec.elementos1903;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Elementos1903Application {
+public class Elementos1903Application implements CommandLineRunner{
 
+    
+    @Autowired
+    RepoCliente repoCliente;
 	public static void main(String[] args) {
 		SpringApplication.run(Elementos1903Application.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+       
+        //repoCliente.save(new Cliente(1,"slayerghost1","Alex@hotmail.com"));
+       //repoCliente.save(new Cliente(2,"topoyin,","slayerghost1@gmail.com"));
+        //repoCliente.save(new Cliente(3,"jesus,","jesusatan@gmail.com"));
+    
+       //buscar todos
+       
+       for(Cliente c: repoCliente.findAll()){
+           System.out.println(c);
+       }
+       
+       //buscar por id
+        System.out.println(repoCliente.findById(2));
+        
+        //borramos el primero despues lo vuelven a insertar
+        
+        //repoCliente.deleteById(1);
+        
+       
+    }
 
 }
